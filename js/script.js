ea28d5a17ee3aa62d82632fadf9064fade092645
nickshri09 <105236667+nickshri09@users.noreply.github.com>
2022-05-13 16:10:02 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,48 @@
+function titleFunction() {
+  x = document.getElementById("title_input").value;
+  document.getElementById("show_title").innerHTML = x;
+}
+
+document.querySelector("#input").addEventListener("keydown", (event) => {
+  if(event.key === "Enter"){
+    const input = document.querySelector("#input");
+    addItem(input.value);
+  }
+    
+});
+
+document.querySelector("#add_item").addEventListener("click", () => {
+  const input = document.querySelector("#input");
+  addItem(input.value);
+});
+
+addItem = (input) => {
+  const item = document.createElement("div");
+  const div = document.createElement("div");
+  const checkIcon = document.createElement("i");
+  const trashIcon = document.createElement("i");
+  const text = document.createElement("p");
+
+  item.className = "item";
+  text.textContent = input;
+
+  checkIcon.className = "fas fa-check-square";
+  checkIcon.style.color = "lightgray";
+  checkIcon.addEventListener("click", () => {
+    checkIcon.style.color = "limegreen";
+  })
+  div.appendChild(checkIcon);
+
+  trashIcon.className = "fas fa-trash";
+  trashIcon.style.color = "darkgray";
+  trashIcon.addEventListener("click", () => {
+    item.remove();
+  })
+  div.appendChild(trashIcon);
+
+  item.appendChild(text);
+  item.appendChild(div);
+
+  document.querySelector("#to_do_list").appendChild(item);
+  document.querySelector("#input").value = "";
+}
